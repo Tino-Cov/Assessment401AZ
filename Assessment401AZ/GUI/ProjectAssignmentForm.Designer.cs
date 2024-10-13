74cfc5b398e88b1f2a583808b60c5cd84b0623c4
Tino-Cov <gambizaa@coventry.ac.uk>
2024-10-13 21:44:22 +0100

    ProjectAssignment GUI done with code

--- Assessment401AZ/GUI/ProjectAssignmentForm.Designer.cs
+++ Assessment401AZ/GUI/ProjectAssignmentForm.Designer.cs
@@ -28,12 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ProjectAssignmentForm";
+            dataGridViewProjectAssignment = new DataGridView();
+            ADD = new Button();
+            Go_To_ProjectFiles = new Button();
+            DELETE = new Button();
+            UPDATE = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtProjectName = new TextBox();
+            txtProjectID = new TextBox();
+            txtProjectAddress = new TextBox();
+            txtManagerID = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProjectAssignment).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewProjectAssignment
+            // 
+            dataGridViewProjectAssignment.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProjectAssignment.Location = new Point(444, 30);
+            dataGridViewProjectAssignment.Name = "dataGridViewProjectAssignment";
+            dataGridViewProjectAssignment.Size = new Size(482, 188);
+            dataGridViewProjectAssignment.TabIndex = 0;
+            // 
+            // ADD
+            // 
+            ADD.Location = new Point(426, 283);
+            ADD.Name = "ADD";
+            ADD.Size = new Size(157, 48);
+            ADD.TabIndex = 1;
+            ADD.Text = "ADD";
+            ADD.UseVisualStyleBackColor = true;
+            // 
+            // Go_To_ProjectFiles
+            // 
+            Go_To_ProjectFiles.Location = new Point(616, 363);
+            Go_To_ProjectFiles.Name = "Go_To_ProjectFiles";
+            Go_To_ProjectFiles.Size = new Size(157, 48);
+            Go_To_ProjectFiles.TabIndex = 2;
+            Go_To_ProjectFiles.Text = "Go to ProjectFiles";
+            Go_To_ProjectFiles.UseVisualStyleBackColor = true;
+            // 
+            // DELETE
+            // 
+            DELETE.Location = new Point(803, 283);
+            DELETE.Name = "DELETE";
+            DELETE.Size = new Size(157, 48);
+            DELETE.TabIndex = 3;
+            DELETE.Text = "DELETE";
+            DELETE.UseVisualStyleBackColor = true;
+            // 
+            // UPDATE
+            // 
+            UPDATE.Location = new Point(616, 283);
+            UPDATE.Name = "UPDATE";
+            UPDATE.Size = new Size(157, 48);
+            UPDATE.TabIndex = 4;
+            UPDATE.Text = "UPDATE";
+            UPDATE.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 15);
+            label1.TabIndex = 5;
+            label1.Text = "ProjectName :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 6;
+            label2.Text = "ProjectID :";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(46, 218);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 7;
+            label3.Text = "ProjectAddress :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(46, 283);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 15);
+            label4.TabIndex = 8;
+            label4.Text = "ManagerID :";
+            label4.Click += label4_Click;
+            // 
+            // txtProjectName
+            // 
+            txtProjectName.Location = new Point(156, 81);
+            txtProjectName.Name = "txtProjectName";
+            txtProjectName.Size = new Size(126, 23);
+            txtProjectName.TabIndex = 9;
+            // 
+            // txtProjectID
+            // 
+            txtProjectID.Location = new Point(156, 149);
+            txtProjectID.Name = "txtProjectID";
+            txtProjectID.Size = new Size(126, 23);
+            txtProjectID.TabIndex = 10;
+            // 
+            // txtProjectAddress
+            // 
+            txtProjectAddress.Location = new Point(156, 210);
+            txtProjectAddress.Name = "txtProjectAddress";
+            txtProjectAddress.Size = new Size(126, 23);
+            txtProjectAddress.TabIndex = 11;
+            // 
+            // txtManagerID
+            // 
+            txtManagerID.Location = new Point(156, 283);
+            txtManagerID.Name = "txtManagerID";
+            txtManagerID.Size = new Size(126, 23);
+            txtManagerID.TabIndex = 12;
+            // 
+            // ProjectAssignmentForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(972, 470);
+            Controls.Add(txtManagerID);
+            Controls.Add(txtProjectAddress);
+            Controls.Add(txtProjectID);
+            Controls.Add(txtProjectName);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(UPDATE);
+            Controls.Add(DELETE);
+            Controls.Add(Go_To_ProjectFiles);
+            Controls.Add(ADD);
+            Controls.Add(dataGridViewProjectAssignment);
+            Name = "ProjectAssignmentForm";
+            Text = "ProjectAssignmentForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProjectAssignment).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridViewProjectAssignment;
+        private Button ADD;
+        private Button Go_To_ProjectFiles;
+        private Button DELETE;
+        private Button UPDATE;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtProjectName;
+        private TextBox txtProjectID;
+        private TextBox txtProjectAddress;
+        private TextBox txtManagerID;
     }
 }
--- Assessment401AZ/GUI/ProjectAssignmentForm.cs
+++ Assessment401AZ/GUI/ProjectAssignmentForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assessment401AZ.DBOperations;
+using Assessment401AZ.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,136 @@ namespace Assessment401AZ.GUI
         public ProjectAssignmentForm()
         {
             InitializeComponent();
+        }
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            ProjectAssignment newProjectAssignment = new ProjectAssignment
+            {
+                ProjectID = txtProjectID.Text,
+                ProjectAddress = txtProjectAddress.Text,
+                ManagerID = txtManagerID.Text,
+            };
+
+            ProjectAssignmentOperations.AddProjectAssignment(newProjectAssignment);
+            LoadProjectAssignments();
+        }
+
+
+        private void LoadProjectAssignments()
+        {
+            dataGridViewProjectAssignment.DataSource = ProjectAssignmentOperations.GetProjectAssignment();
+        }
+
+        private void UPDATE_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewProjectAssignment.SelectedRows.Count > 0)
+            {
+                ProjectAssignment selectedProjectAssignment = GetSelectedRowProjectAssignment();
+
+                if (selectedProjectAssignment != null)
+                {
+                    selectedProjectAssignment.ProjectName = txtProjectName.Text;
+                    selectedProjectAssignment.ProjectAddress = txtProjectAddress.Text;
+                    selectedProjectAssignment.ManagerID = txtManagerID.Text;
+
+                    ProjectAssignmentOperations.UpdateProjectAssignment(selectedProjectAssignment);
+                    LoadProjectAssignments();
+                    ClearProjectAssignmentFields();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a ProjectAssignment to update.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a ProjectAssignment to update.");
+            }
+        }
+
+        private ProjectAssignment GetSelectedRowProjectAssignment()
+        {
+            if (dataGridViewProjectAssignment.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewProjectAssignment.SelectedRows[0];
+                return new ProjectAssignment
+                {
+                    ProjectName = Convert.ToInt32(selectedRow.Cells["ProjectName"].Value),
+                    ProjectID = Convert.ToInt32(selectedRow.Cells["ProjectID"].Value),
+                    ProjectAddress = Convert.ToString(selectedRow.Cells["ProjectAddress"].Value),
+                    ManagerID = Convert.ToInt32(selectedRow.Cells["ManagerID"].Value),
+
+                };
+            }
+
+            return null;
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewProjectAssignment.SelectedRows.Count > 0)
+            {
+                int ProjectAssignmentId = GetSelectedRowProjectAssignmentId();
+
+                if (ProjectAssignmentId != -1)
+                {
+                    ProjectAssignmentOperations.DeleteProjectAssignment(ProjectAssignmentId);
+                    LoadProjectAssignments();
+                    ClearProjectAssignmentFields();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a ProjectAssignment to delete.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a ProjectAssignment to delete.");
+            }
+        }
+
+        private void Go_To_ProjectFiles_Click(object sender, EventArgs e)
+        {
+            EngineersForm EngineersForm = new EngineersForm();
+            EngineersForm.Show();
+
+        }
+
+        private void ProjectAssignmentForm_Load_1(object sender, EventArgs e)
+        {
+            LoadProjectAssignments();
+        }
+
+        private void ClearItemFields()
+        {
+            txtProjectName.Clear();
+            txtProjectAddress.Clear();
+            txtManagerID.Clear();
+        }
+
+
+
+        private int GetSelectedRowProjectAssignmentId()
+        {
+            if (dataGridViewProjectAssignment.SelectedRows.Count > 0)
+            {
+                return Convert.ToInt32(dataGridViewProjectAssignment.SelectedRows[0].Cells["ProjectName"].Value);
+            }
+
+            return -1;
+        }
+
+        private void ClearProjectAssignmentFields()
+        {
+            txtProjectName.Clear();
+            txtProjectAddress.Clear();
+            txtManagerID.Clear();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
