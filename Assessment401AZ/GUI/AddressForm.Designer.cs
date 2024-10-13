51b9bb149fb659b2d06f906805c518ad63bc3833
Tino-Cov <gambizaa@coventry.ac.uk>
2024-10-13 20:32:01 +0100

    Address GUI created with code
    Same Issue after Code was Made

--- Assessment401AZ/GUI/AddressForm.Designer.cs
+++ Assessment401AZ/GUI/AddressForm.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddressForm";
+            dataGridViewAddress = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtAddressID = new TextBox();
+            txtStreet = new TextBox();
+            txtCity = new TextBox();
+            txtPostCode = new TextBox();
+            ADD = new Button();
+            DELETE = new Button();
+            UPDATE = new Button();
+            GoToEngineersForm = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewAddress).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewAddress
+            // 
+            dataGridViewAddress.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewAddress.Location = new Point(367, 53);
+            dataGridViewAddress.Name = "dataGridViewAddress";
+            dataGridViewAddress.Size = new Size(371, 163);
+            dataGridViewAddress.TabIndex = 0;
+            dataGridViewAddress.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(52, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 1;
+            label1.Text = "AddressID : ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(52, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Street :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(52, 178);
+            label3.Name = "label3";
+            label3.Size = new Size(34, 15);
+            label3.TabIndex = 3;
+            label3.Text = "City :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(52, 218);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 15);
+            label4.TabIndex = 4;
+            label4.Text = "PostCode :";
+            // 
+            // txtAddressID
+            // 
+            txtAddressID.Location = new Point(150, 95);
+            txtAddressID.Name = "txtAddressID";
+            txtAddressID.Size = new Size(133, 23);
+            txtAddressID.TabIndex = 5;
+            // 
+            // txtStreet
+            // 
+            txtStreet.Location = new Point(150, 137);
+            txtStreet.Name = "txtStreet";
+            txtStreet.Size = new Size(133, 23);
+            txtStreet.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            txtCity.Location = new Point(150, 175);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(133, 23);
+            txtCity.TabIndex = 7;
+            // 
+            // txtPostCode
+            // 
+            txtPostCode.Location = new Point(150, 215);
+            txtPostCode.Name = "txtPostCode";
+            txtPostCode.Size = new Size(133, 23);
+            txtPostCode.TabIndex = 8;
+            // 
+            // ADD
+            // 
+            ADD.Location = new Point(317, 267);
+            ADD.Name = "ADD";
+            ADD.Size = new Size(133, 42);
+            ADD.TabIndex = 9;
+            ADD.Text = "ADD";
+            ADD.UseVisualStyleBackColor = true;
+            // 
+            // DELETE
+            // 
+            DELETE.Location = new Point(644, 267);
+            DELETE.Name = "DELETE";
+            DELETE.Size = new Size(133, 42);
+            DELETE.TabIndex = 10;
+            DELETE.Text = "DELETE";
+            DELETE.UseVisualStyleBackColor = true;
+            // 
+            // UPDATE
+            // 
+            UPDATE.Location = new Point(486, 267);
+            UPDATE.Name = "UPDATE";
+            UPDATE.Size = new Size(133, 42);
+            UPDATE.TabIndex = 11;
+            UPDATE.Text = "UPDATE";
+            UPDATE.UseVisualStyleBackColor = true;
+            // 
+            // GoToEngineersForm
+            // 
+            GoToEngineersForm.Location = new Point(486, 361);
+            GoToEngineersForm.Name = "GoToEngineersForm";
+            GoToEngineersForm.Size = new Size(133, 42);
+            GoToEngineersForm.TabIndex = 12;
+            GoToEngineersForm.Text = "Go To EngineersForm";
+            GoToEngineersForm.UseVisualStyleBackColor = true;
+            // 
+            // AddressForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(GoToEngineersForm);
+            Controls.Add(UPDATE);
+            Controls.Add(DELETE);
+            Controls.Add(ADD);
+            Controls.Add(txtPostCode);
+            Controls.Add(txtCity);
+            Controls.Add(txtStreet);
+            Controls.Add(txtAddressID);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridViewAddress);
+            Name = "AddressForm";
+            Text = "AddressForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewAddress).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridViewAddress;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtAddressID;
+        private TextBox txtStreet;
+        private TextBox txtCity;
+        private TextBox txtPostCode;
+        private Button ADD;
+        private Button DELETE;
+        private Button UPDATE;
+        private Button GoToEngineersForm;
     }
 }
--- Assessment401AZ/GUI/AddressForm.cs
+++ Assessment401AZ/GUI/AddressForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assessment401AZ.DBOperations;
+using Assessment401AZ.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,137 @@ namespace Assessment401AZ.GUI
         public AddressForm()
         {
             InitializeComponent();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            Address newAddress = new Address
+            {
+                Street = txtStreet.Text,
+                City = txtCity.Text,
+                PostCode = txtPostCode.Text,
+            };
+
+            adressOperations.AddAddress(newAddress);
+            LoadAddresss();
+        }
+
+
+        private void LoadAddresss()
+        {
+            dataGridViewAddress.DataSource = addressOperations.GetAddresss();
+        }
+
+        private void UPDATE_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewAddress.SelectedRows.Count > 0)
+            {
+                Address selectedAddress = GetSelectedRowAddress();
+
+                if (selectedAddress != null)
+                {
+                    selectedAddress.Street = txtStreet.Text;
+                    selectedAddress.City = txtCity.Text;
+                    selectedAddress.PostCode = txtPostCode.Text;
+
+                    addressOperations.UpdateAddress(selectedAddress);
+                    LoadAddresss();
+                    ClearAddressFields();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a Address to update.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a Address to update.");
+            }
+        }
+
+        private Address GetSelectedRowAddress()
+        {
+            if (dataGridViewAddress.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewAddress.SelectedRows[0];
+                return new Address 
+                {
+                    Id = Convert.ToInt32(selectedRow.Cells["AddressID"].Value),
+                    Street = Convert.ToString(selectedRow.Cells["Street"].Value),
+                    City = Convert.ToString(selectedRow.Cells["City"].Value),
+                    PostCode = Convert.ToString(selectedRow.Cells["PostCode"].Value)
+
+                };
+            }
+
+            return null;
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewAddress.SelectedRows.Count > 0)
+            {
+                int AddressId = GetSelectedRowAddressId();
+
+                if (AddressId != -1)
+                {
+                    addressOperations.DeleteAddress(AddressId);
+                    LoadAddresss();
+                    ClearAddressFields();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a Address to delete.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a Address to delete.");
+            }
+        }
+
+        private void Go_To_EngineersForm_Click(object sender, EventArgs e)
+        {
+            EngineersForm EngineersForm = new EngineersForm();
+            EngineersForm.Show();
+
+        }
+
+        private void AddressForm_Load_1(object sender, EventArgs e)
+        {
+            LoadAddresss();
+        }
+
+        private void ClearItemFields()
+        {
+            txtStreet.Clear();
+            txtCity.Clear();
+            txtPostCode.Clear();
+        }
+
+
+
+        private int GetSelectedRowAddressId()
+        {
+            if (dataGridViewAddress.SelectedRows.Count > 0)
+            {
+                return Convert.ToInt32(dataGridViewAddress.SelectedRows[0].Cells["AddressID"].Value);
+            }
+
+            return -1;
+        }
+
+        private void ClearAddressFields()
+        {
+            txtStreet.Clear();
+            txtCity.Clear();
+            txtPostCode.Clear();
+            txtPostCode.Clear();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
